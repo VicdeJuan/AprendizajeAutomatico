24d40d680f5de0192c0ca2069252aaf1b91df01b
VicdeJuan <dejuan.victor@gmail.com>
2015-10-06 11:28:21 +0200

    NaiveBayes para rellenar

---
+++ P1/src/clasificadores/ClasificadorNaiveBayes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasificadores;
+
+import datos.Datos;
+import java.util.HashMap;
+
+/**
+ *
+ * @author vicdejuan
+ */
+public class ClasificadorNaiveBayes extends Clasificador {
+    
+    @Override
+    public void entrenamiento(Datos datosTrain) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<String, Integer> clasifica(Datos datosTest) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

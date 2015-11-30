5c084087d5f9d3bbc9fd2125e94e26fd4d8eaf10
VicdeJuan <dejuan.victor@gmail.com>
2015-11-30 17:01:02 +0100

    Corregido un error del script para gráficas

--- Algoritmos/scripting/genGraph.rb
+++ Algoritmos/scripting/genGraph.rb
@@ -3,24 +3,20 @@ outputdir=ARGV[1]
 
 f = File.open(file, "r")
 a = [0,0,0,0]
-b=[[]]
 i=0
 j=0
+tow_file = outputdir + "/"+file.split(".")[0].split("/")[1]
+tow = File.open(tow_file,'w')
+tow.print "max medio min\n"
 f.readlines().each do |line|
 	val = line.split(":")[1].to_f
 	a[i] = val
-	i=(i+1)%4
+	i=(i+1)%5
 	if i==0
-		b[j] = a
 		j+=1
+		tow.print "#{a[1]} #{a[2]} #{a[3]}\n"
 	end
 end
-
-tow_file = outputdir + "/"+file.split(".")[0].split("/")[1]
-tow = File.open(tow_file,'w')
-
-tow.print "max medio min\n"
-b.map { |a| tow.print "#{a[1]} #{a[2]} #{a[3]}\n"}
 
 f.close()
 tow.close()
